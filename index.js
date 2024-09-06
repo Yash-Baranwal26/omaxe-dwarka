e9c642fd5726f8d4a029519740fd9e0f215a8ff0
Yash-Baranwal26 <yashbaranwal121@gmail.com>
2024-09-06 14:05:51 +0530

    env file created

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 const express = require('express');
 const users = require('./users');
 const cors = require('cors')
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors());
 
-mongoose.connect('mongodb+srv://yashbaranwal121:yash2612@cluster0.c49ls.mongodb.net/omaxeDwarka?retryWrites=true&w=majority&appName=Cluster0')
+app.use(cors({
+    origin: ['https://omaxemallsector19.in', 'http://localhost:3000'], // Replace with your actual frontend domain
+    optionsSuccessStatus: 200,
+    credentials: true // If you are using cookies, you need to set credentials to true
+  }));
+
+// app.use((req, res, next) => {
+//     res.cookie('example', 'value', {
+//         httpOnly: true,
+//         secure: true, // Ensure cookies are only sent over HTTPS
+//         sameSite: 'Lax' // Adjust this based on your needs ('Strict' or 'None' with Secure)
+//     });
+//     next();
+// });
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("DB Connected");
 })                  
